9aca151b7614da590e8014f84ffaa1750ca7cea7
ssenyonjoalvin <alvinssenyonjo9@gmail.com>
2026-08-17 14:36:23 +0300

    i don't know what is happening

--- prisma/migrations/20260817120000_add_order_reference_images/migration.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `cake_orders`
-ADD COLUMN `referenceImages` JSON NOT NULL DEFAULT (JSON_ARRAY());
-
-UPDATE `cake_orders`
-SET `referenceImages` = JSON_ARRAY()
-WHERE `referenceImages` IS NULL;
